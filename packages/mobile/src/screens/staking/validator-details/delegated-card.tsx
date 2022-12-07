fc3a06318d2b65079c47e8fe8fe813690006ee55
Khanh Vo <khanh.vo@tiki.vn>
2022-12-07 12:28:50 +0700

    Update staking UI

--- packages/mobile/src/screens/staking/validator-details/delegated-card.tsx
+++ packages/mobile/src/screens/staking/validator-details/delegated-card.tsx
@@ -1,25 +1,22 @@
-import React, { FunctionComponent, useMemo, useState } from "react";
+import { CoinPretty, Dec } from "@keplr-wallet/unit";
 import { observer } from "mobx-react-lite";
+import React, { FunctionComponent, useMemo, useState } from "react";
+import { useIntl } from "react-intl";
 import { Text, View, ViewStyle } from "react-native";
-import { Card, CardBody, CardDivider } from "../../../components/card";
+import { formatCoin } from "../../../common/utils";
+import { Button, CannotRedelegateIcon } from "../../../components";
+import { Card } from "../../../components/card";
+import { registerModal } from "../../../modals/base";
 import { useSmartNavigation } from "../../../navigation-util";
 import { useStore } from "../../../stores";
 import { useStyle } from "../../../styles";
-import { FormattedMessage, useIntl } from "react-intl";
-import { formatCoin } from "../../../common/utils";
-import { registerModal } from "../../../modals/base";
-import {
-  Button,
-  CannotRedelegateIcon,
-  DelegateIcon,
-  RedelegateIcon,
-  UndelegateIcon,
-} from "../../../components";
+import { PropertyView, PropertyViewIconType } from "../component/property";
 
 export const DelegatedCard: FunctionComponent<{
   containerStyle?: ViewStyle;
   validatorAddress: string;
-}> = observer(({ containerStyle, validatorAddress }) => {
+  followHandler?: () => void;
+}> = observer(({ containerStyle, validatorAddress, followHandler }) => {
   const { chainStore, queriesStore, accountStore } = useStore();
 
   const account = accountStore.getAccount(chainStore.current.chainId);
@@ -63,191 +60,151 @@ export const DelegatedCard: FunctionComponent<{
     });
   }, [redelegation]);
 
+  const unbondingsQuery = queries.cosmos.queryUnbondingDelegations.getQueryBech32Address(
+    account.bech32Address
+  );
+
+  const unbondings = unbondingsQuery.unbondingBalances;
+
+  const zeroAmount = new CoinPretty(staked.currency, new Dec(0));
+  const unbonding = unbondings.find(
+    (unbonding) => validatorAddress === unbonding.validatorAddress
+  );
+  const unbondingAmount =
+    unbonding?.entries.reduce((coin, entry) => {
+      return coin.add(entry.balance);
+    }, zeroAmount) || zeroAmount;
+
   return (
     <Card style={containerStyle}>
-      <CardBody style={style.flatten(["padding-y-0"])}>
+      <View
+        style={style.flatten([
+          "padding-x-12",
+          "padding-y-16",
+          "background-color-card-background",
+          "border-radius-16",
+        ])}
+      >
+        <PropertyView
+          iconType={PropertyViewIconType.staked}
+          label={intl.formatMessage({
+            id: "validator.details.delegated.invested",
+          })}
+          value={formatCoin(staked, false, 2)}
+          labelStyle={style.flatten(["color-staking-staked-text"])}
+        />
+        <Button
+          size="medium"
+          text={intl.formatMessage({
+            id: "validator.details.delegated.investMore",
+          })}
+          containerStyle={style.flatten(["margin-y-8"])}
+          onPress={() => {
+            smartNavigation.navigateSmart("Delegate", {
+              validatorAddress,
+            });
+          }}
+        />
+        <View
+          style={style.flatten(["margin-y-0", "flex-row", "justify-center"])}
+        >
+          <Button
+            size="medium"
+            mode="outline"
+            text={intl.formatMessage({
+              id: "validator.details.delegated.regelegate",
+            })}
+            containerStyle={style.flatten(["flex-1"])}
+            onPress={() => {
+              if (!redelegation) {
+                smartNavigation.navigateSmart("Redelegate", {
+                  validatorAddress,
+                });
+                return;
+              }
+
+              setDisplayCannotRedelegateModal(true);
+            }}
+          />
+          <View style={style.flatten(["width-8"])} />
+          <Button
+            size="medium"
+            mode="outline"
+            text={intl.formatMessage({
+              id: "validator.details.delegated.undelegate",
+            })}
+            containerStyle={style.flatten(["flex-1"])}
+            onPress={() => {
+              smartNavigation.navigateSmart("Undelegate", {
+                validatorAddress,
+              });
+            }}
+          />
+        </View>
         <View
           style={style.flatten([
-            "padding-0",
-            "background-color-card-background",
-            "border-radius-16",
-            "border-color-card-border",
-            "border-width-1",
+            "height-1",
+            "background-color-card-separator",
+            "margin-y-16",
           ])}
-        >
-          <View
-            style={style.flatten(["margin-y-0", "flex-row", "justify-center"])}
-          >
-            <View
-              style={style.flatten([
-                "flex-1",
-                "margin-left-0",
-                "items-center",
-                "padding-x-16",
-                "padding-y-24",
-              ])}
-            >
-              <Text
-                style={style.flatten(["color-gray-30", "text-small-medium"])}
-              >
-                <FormattedMessage id="validator.details.delegated.invested" />
-              </Text>
-              <Text
-                style={style.flatten([
-                  "color-gray-10",
-                  "text-medium-medium",
-                  "margin-top-2",
-                ])}
-              >
-                {formatCoin(staked)}
-              </Text>
-            </View>
-            <View
-              style={style.flatten([
-                "background-color-gray-70",
-                "margin-y-0",
-                "width-1",
-              ])}
-            />
-            <View
-              style={style.flatten([
-                "flex-1",
-                "margin-left-0",
-                "items-center",
-                "padding-x-16",
-                "padding-y-24",
-              ])}
-            >
-              <Text
-                style={style.flatten(["color-gray-30", "text-small-medium"])}
-              >
-                <FormattedMessage id="validator.details.delegated.profit" />
-              </Text>
-              <Text
-                style={style.flatten([
-                  "color-rewards-text",
-                  "text-medium-medium",
-                  "margin-top-2",
-                ])}
-              >
-                {"+" + formatCoin(rewards)}
-              </Text>
-            </View>
-          </View>
-          <CardDivider
-            style={style.flatten(["background-color-gray-70", "margin-x-0"])}
+        />
+        <View style={style.flatten(["flex-row", "items-start"])}>
+          <PropertyView
+            iconType={PropertyViewIconType.rewards}
+            label={intl.formatMessage({
+              id: "validator.details.delegated.profit",
+            })}
+            value={"+" + formatCoin(rewards, false, 4)}
+            labelStyle={style.flatten(["color-staking-rewards-text"])}
           />
-          <View style={style.flatten(["margin-y-16", "flex-row"])}>
-            <View
-              style={style.flatten(["items-center", "flex-1", "padding-x-4"])}
-            >
-              <Button
-                color="neutral"
-                text=""
-                leftIcon={<UndelegateIcon />}
-                onPress={() => {
-                  smartNavigation.navigateSmart("Undelegate", {
-                    validatorAddress,
-                  });
-                }}
-                containerStyle={style.flatten([
-                  "border-radius-22",
-                  "width-44",
-                  "padding-x-0",
-                ])}
-              />
+          <View style={style.flatten(["width-8"])} />
+          <View style={style.flatten(["flex-1"])}>
+            <PropertyView
+              iconType={PropertyViewIconType.unbonding}
+              label={intl.formatMessage(
+                { id: "validator.details.delegated.unbonding" },
+                { denom: unbondingAmount.denom }
+              )}
+              value={formatCoin(unbondingAmount, false, 2)}
+              labelStyle={style.flatten(["color-staking-unbonding-text"])}
+            />
+            {unbondingAmount.toDec().isPositive() && (
               <Text
                 style={style.flatten([
-                  "color-label-text-1",
-                  "text-small-regular",
-                  "margin-top-8",
-                  "text-center",
+                  "color-link-text",
+                  "text-small-medium",
+                  "margin-top-2",
                 ])}
-              >
-                <FormattedMessage id="validator.details.delegated.undelegate" />
-              </Text>
-            </View>
-            <View
-              style={style.flatten(["items-center", "flex-1", "padding-x-4"])}
-            >
-              <Button
-                color="neutral"
-                text=""
-                leftIcon={<RedelegateIcon />}
                 onPress={() => {
-                  if (!redelegation) {
-                    smartNavigation.navigateSmart("Redelegate", {
-                      validatorAddress,
-                    });
-                    return;
+                  if (followHandler) {
+                    followHandler();
                   }
-
-                  setDisplayCannotRedelegateModal(true);
                 }}
-                containerStyle={style.flatten([
-                  "border-radius-22",
-                  "width-44",
-                  "padding-x-0",
-                ])}
-              />
-              <Text
-                style={style.flatten([
-                  "color-label-text-1",
-                  "text-small-regular",
-                  "margin-top-8",
-                  "text-center",
-                ])}
               >
-                <FormattedMessage id="validator.details.delegated.regelegate" />
-              </Text>
-              <CannotRedelegateModal
-                isOpen={displayCannotRedelegateModal}
-                close={() => {
-                  setDisplayCannotRedelegateModal(false);
-                }}
-                title={intl.formatMessage({
-                  id: "common.modal.cannotRedelegate.title",
+                {intl.formatMessage({
+                  id: "common.text.follow",
                 })}
-                content={intl.formatMessage(
-                  { id: "common.modal.cannotRedelegate.content" },
-                  { date: redelegationCompletionTime }
-                )}
-                buttonText={intl.formatMessage({
-                  id: "common.text.understand",
-                })}
-              />
-            </View>
-            <View
-              style={style.flatten(["items-center", "flex-1", "padding-x-4"])}
-            >
-              <Button
-                text=""
-                leftIcon={<DelegateIcon />}
-                onPress={() => {
-                  smartNavigation.navigateSmart("Delegate", {
-                    validatorAddress,
-                  });
-                }}
-                containerStyle={style.flatten([
-                  "border-radius-22",
-                  "width-44",
-                  "padding-x-0",
-                ])}
-              />
-              <Text
-                style={style.flatten([
-                  "color-label-text-1",
-                  "text-small-regular",
-                  "margin-top-8",
-                  "text-center",
-                ])}
-              >
-                <FormattedMessage id="validator.details.delegated.investMore" />
               </Text>
-            </View>
+            )}
           </View>
         </View>
-      </CardBody>
+        <CannotRedelegateModal
+          isOpen={displayCannotRedelegateModal}
+          close={() => {
+            setDisplayCannotRedelegateModal(false);
+          }}
+          title={intl.formatMessage({
+            id: "common.modal.cannotRedelegate.title",
+          })}
+          content={intl.formatMessage(
+            { id: "common.modal.cannotRedelegate.content" },
+            { date: redelegationCompletionTime }
+          )}
+          buttonText={intl.formatMessage({
+            id: "common.text.understand",
+          })}
+        />
+      </View>
     </Card>
   );
 });
--- packages/mobile/src/screens/staking/validator-details/header-card.tsx
+++ packages/mobile/src/screens/staking/validator-details/header-card.tsx
@@ -1,3 +1,4 @@
+import { Staking } from "@keplr-wallet/stores";
 import { observer } from "mobx-react-lite";
 import React, { Fragment, FunctionComponent } from "react";
 import { useIntl } from "react-intl";
@@ -11,13 +12,27 @@ import {
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { LeftArrowIcon } from "../../../components";
 import { Card } from "../../../components/card";
+import { ValidatorThumbnail } from "../../../components/thumbnail";
 import { useSmartNavigation } from "../../../navigation-util";
+import { useStore } from "../../../stores";
 import { useStyle } from "../../../styles";
 
 export const ValidatorHeaderCard: FunctionComponent<{
+  validatorAddress: string;
   containerStyle?: ViewStyle;
   animOpacity: Animated.Value;
-}> = observer(({ containerStyle, animOpacity }) => {
+}> = observer(({ validatorAddress, containerStyle, animOpacity }) => {
+  const { chainStore, queriesStore } = useStore();
+
+  const queries = queriesStore.get(chainStore.current.chainId);
+
+  const queryValidators = queries.cosmos.queryValidators.getQueryStatus(
+    Staking.BondStatus.Unspecified
+  );
+
+  const thumbnail = queryValidators.getValidatorThumbnail(validatorAddress);
+  const validator = queryValidators.getValidator(validatorAddress);
+
   const inl = useIntl();
   const safeAreaInsets = useSafeAreaInsets();
   const style = useStyle();
@@ -29,9 +44,69 @@ export const ValidatorHeaderCard: FunctionComponent<{
     }),
   };
 
+  const getNavigationBar = (
+    title: string,
+    icon?: {
+      url?: string;
+      hidden?: boolean;
+    }
+  ) => {
+    return (
+      <View
+        style={{
+          ...style.flatten([
+            "flex-row",
+            "padding-x-16",
+            "height-44",
+            "justify-center",
+            "items-center",
+          ]),
+          marginTop: safeAreaInsets.top,
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            smartNavigation.goBack();
+          }}
+        >
+          <LeftArrowIcon size={24} color={style.get("color-white").color} />
+        </TouchableOpacity>
+        <View
+          style={{
+            ...style.flatten(
+              ["flex-1", "flex-row", "items-center", "margin-right-24"],
+              [icon?.hidden !== true ? "justify-start" : "justify-center"]
+            ),
+          }}
+        >
+          {icon?.hidden !== true ? (
+            <ValidatorThumbnail
+              size={24}
+              url={icon?.url}
+              style={style.flatten(["margin-x-8"])}
+            />
+          ) : null}
+          <Text
+            style={style.flatten([
+              "text-center",
+              "color-white",
+              "text-large-bold",
+            ])}
+          >
+            {title}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <Card style={containerStyle}>
       <Fragment>
+        {getNavigationBar(
+          inl.formatMessage({ id: "validator.details.new.title" }),
+          { hidden: true }
+        )}
         <Animated.View
           style={[
             {
@@ -41,41 +116,11 @@ export const ValidatorHeaderCard: FunctionComponent<{
             },
             viewAnimOpacity,
           ]}
-        />
-        <View
-          style={{
-            ...style.flatten([
-              "flex-row",
-              "padding-x-16",
-              "height-44",
-              "justify-center",
-              "items-center",
-            ]),
-            marginTop: safeAreaInsets.top,
-          }}
         >
-          <TouchableOpacity
-            onPress={() => {
-              smartNavigation.goBack();
-            }}
-          >
-            <LeftArrowIcon size={24} color={style.get("color-white").color} />
-          </TouchableOpacity>
-          <View
-            style={{
-              ...style.flatten([
-                "flex-1",
-                "justify-center",
-                "items-center",
-                "margin-right-24",
-              ]),
-            }}
-          >
-            <Text style={style.flatten(["text-center", "color-white", "h5"])}>
-              {inl.formatMessage({ id: "validator.details.new.title" })}
-            </Text>
-          </View>
-        </View>
+          {getNavigationBar(validator?.description.moniker || "", {
+            url: thumbnail,
+          })}
+        </Animated.View>
       </Fragment>
     </Card>
   );
