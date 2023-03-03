136c72b9d9373f4814982cb9ca9631b86cecf788
Khanh Vo <khanh.vo@tiki.vn>
2023-03-03 21:59:22 +0700

    Add codepush label

--- packages/mobile/src/screens/settings/items/version-item.tsx
+++ packages/mobile/src/screens/settings/items/version-item.tsx
@@ -1,17 +1,31 @@
 import { observer } from "mobx-react-lite";
-import { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { Text, View } from "react-native";
-import { useStyle } from "../../../styles";
+import CodePush, { LocalPackage } from "react-native-code-push";
 import DeviceInfo from "react-native-device-info";
-import { useState } from "react";
-import React from "react";
+import { useStyle } from "../../../styles";
 export const AccountVersionItem: FunctionComponent = observer(() => {
-    const style = useStyle();
-    const [appVersion] = useState(() => DeviceInfo.getVersion());
-    const [buildNumber] = useState(() => DeviceInfo.getBuildNumber());
-    return (
-        <View style={style.flatten(["height-48", "items-center"])}>
-            <Text style={style.flatten(["body3", "color-text-black-low"])}>Version {appVersion}({buildNumber})</Text>
-        </View>
-        );
+  const style = useStyle();
+  const [appVersion] = useState(() => DeviceInfo.getVersion());
+  const [buildNumber] = useState(() => DeviceInfo.getBuildNumber());
+  const [codePushLabel, setCodePushLabel] = useState("");
+
+  useEffect(() => {
+    CodePush.getUpdateMetadata(CodePush.UpdateState.RUNNING)
+      .then((status) => {
+        setCodePushLabel((status as LocalPackage)?.label ?? "");
+      })
+      .catch((e) => {
+        console.log("__ERROR_CodePush__", e);
+      });
+  }, []);
+
+  return (
+    <View style={style.flatten(["height-48", "items-center"])}>
+      <Text style={style.flatten(["body3", "color-text-black-low"])}>
+        Version {appVersion} ({buildNumber})
+        {codePushLabel.length != 0 ? ` - ${codePushLabel}` : ""}
+      </Text>
+    </View>
+  );
 });
